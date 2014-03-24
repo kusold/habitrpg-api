03ee126b5f77ee617bae72fc591e94355dbc0cf7
Mike Kusold <git@mikekusold.com>
2014-03-24 13:52:51 -0600

    Initial work on the API, and placeholders for all tests that are needed"

--- tests/apiv2Tests.js
+++ tests/apiv2Tests.js
@@ -13,7 +13,139 @@ describe('HabitRPG API V2 Tests', function() {
   });
 
   it('gets the api docs', function (done) {
-    console.log('works')
-    done();
-  })
-})
+    api.getApiDocs(function(error, res) {
+        expect(error).to.not.exist;
+        expect(res).to.exist;
+        expect(res.statusCode).to.equal(200);
+        // Currently this API Endpoint doesn't return anything
+        expect(res.body).to.be.empty;
+        done();
+    });
+  });
+
+  it('gets the api server status', function (done) {
+    api.getStatus(function(error, res) {
+        expect(error).to.not.exist;
+        expect(res).to.exist;
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.deep.equal({status: 'up'});
+        done();
+    });
+  });
+
+  it('gets all available content objects', function (done) {
+    api.getContent(function(error, res) {
+        expect(error).to.not.exist;
+        expect(res).to.exist;
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.have.property('gear');
+        expect(res.body).to.have.property('potion');
+        expect(res.body).to.have.property('classes');
+        expect(res.body).to.have.property('gearTypes');
+        expect(res.body).to.have.property('spells');
+        expect(res.body).to.have.property('special');
+        expect(res.body).to.have.property('dropEggs');
+        expect(res.body).to.have.property('questEggs');
+        expect(res.body).to.have.property('eggs');
+        expect(res.body).to.have.property('specialPets');
+        expect(res.body).to.have.property('specialMounts');
+        expect(res.body).to.have.property('hatchingPotions');
+        expect(res.body).to.have.property('pets');
+        expect(res.body).to.have.property('questPets');
+        expect(res.body).to.have.property('food');
+        expect(res.body).to.have.property('quests');
+        expect(res.body).to.have.property('userDefaults');
+        done();
+    });
+  });
+
+
+  it('gets the user history for export', function (done) {
+    api.getHistory(function(error, res) {
+        expect(error).to.not.exist;
+        expect(res).to.exist;
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.empty;
+        expect(res.text).to.not.be.empty;
+        done();
+    });
+  });
+
+  it("gets a member's foo");
+
+  describe("User API", function() {
+    it("posts a user's task score");
+    it("gets all user's tasks");
+    it("posts a new task to create");
+    it("gets an individual task");
+    it("puts a user's task to update");
+    it("deletes a task");
+    it("posts the sort order of tasks");
+    it("posts to clear completed tasks");
+    it("posts to unlink a task from its challenge");
+    it("posts to buy a gear piece and equip it automatically");
+    it("posts to sell inventory items back to Alexander");
+    it("posts to purchase a gem-purchaseable item from Alexander");
+    it("posts to feed your pet some food");
+    it("posts to equip an item (either pets, mounts, or gear)");
+    it("posts to pour a hatching potion on an egg");
+    it("gets the full user object");
+    it("puts to update the user object (only certain attributes are supported)");
+    it("deletes a user object entirely");
+    it("posts to revive your dead user");
+    it("posts to drink the fortify potion");
+    it("posts to completely reset your account");
+    it("posts to toggle whether you are resting in the inn");
+    it("posts to rebirth your avatar");
+    it("posts to either remove your avatar's class, or change it to something new");
+    it("posts to allocate one point towards an attribute");
+    it("posts to cast a spell on a target");
+    it("posts to unlock a certain gem-purchaseable path (or multiple paths)");
+    it("posts to buy gems - NOT SUPPORTED");
+    it("posts to cancel subscription - NOT SUPPORTED");
+    it("posts to buy gems with paypal - NOT SUPPORTED");
+    it("posts to batch update a user");
+    it("posts to create a new tag");
+    it("puts to edit a tag");
+    it("deletes a tag");
+  });
+
+  describe('Groups API', function(){
+    it("gets a list of groups");
+    it("posts to create a group");
+    it("gets a group");
+    it("posts to edit a group");
+    it("posts to join a group");
+    it("posts to leave a group");
+    it("posts to invite a user to a group");
+    it("posts to remove a user from a group")
+    it("posts to accept a quest invitation");
+    it("posts to reject a quest invitation");
+    it("posts to abort a quest");
+    it("gets all chat messages");
+    it("posts to send a chat message");
+    it("posts to flag chat messages for a particular group as seen");
+    it("deletes a group");
+    it("posts to like a chat message");
+  });
+
+  describe('Hall API', function() {
+    it('gets a list of the users in the hall of heroes');
+    it("gets a user that is in the hall of heroes");
+    it("adds a user to the hall of heroes");
+    it("gets a list of the patrons for the hall of heroes");
+  });
+
+  describe('Challenges API', function() {
+    it("gets a list of challenges");
+    it("posts to create a challenge");
+    it("gets a challenge");
+    it("gets a challenge in csv format");
+    it("puts to update a challenge");
+    it("deletes a challenge");
+    it("posts to close a challenge");
+    it("posts to join a challenge");
+    it("posts to leave a challenge");
+    it("gets a user's progress on a particular challege");
+  });
+});
