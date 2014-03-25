6d4c75ee56d355549339ede3723b4e1cd9bad32c
Mike Kusold <git@mikekusold.com>
2014-03-24 22:39:35 -0600

    Adding JSHint to the project

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 var apiSettings = {
   apiKey: 'c27a901d-3a43-4393-8ad4-ec87d9c5530f',
   userId: '12048ca1-a0f1-4303-baa3-9f863c09787b'
-}
+};
 
 var config = {
   host: 'localhost',
   port: 3000,
   apiUrl: '/api/v2'
-}
+};
 
 function apiHostUrl(config) {
   return config.host + ":" + config.port + config.apiUrl;
